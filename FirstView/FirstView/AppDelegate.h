cc1fa7b7eaec18d571379a99d3e360bdbac537e8
Takumi ITO <e125743@nw1243.st.ie.u-ryukyu.ac.jp>
2013-11-14 11:30:46 +0900

    ito1 project

---
+++ FirstView/FirstView/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FirstView
+//
+//  Created by Takumi ITO on 2013/11/07.
+//  Copyright (c) 2013年 Takumi ITO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FirstView/FirstViewTests/FirstViewTests.m
@@ -0,0 +1,34 @@
+//
+//  FirstViewTests.m
+//  FirstViewTests
+//
+//  Created by Takumi ITO on 2013/11/07.
+//  Copyright (c) 2013年 Takumi ITO. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface FirstViewTests : XCTestCase
+
+@end
+
+@implementation FirstViewTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
