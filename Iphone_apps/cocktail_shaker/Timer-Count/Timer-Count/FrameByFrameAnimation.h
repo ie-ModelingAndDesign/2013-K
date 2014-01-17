eb79bb0757b9b1bae62b0960c739a9cfff4f1141
たかひろ　あふそ <e125756@takahiroafuso-no-MacBook-Pro.local>
2014-01-17 13:50:44 +0900

    updata

---
+++ Iphone_apps/cocktail_shaker/Timer-Count/Timer-Count/FrameByFrameAnimation.h
@@ -0,0 +1,19 @@
+//
+//  FrameByFrameAnimation.h
+//  FrameByFrameAnimation
+//
+//  Created by 廣川政樹 on 2013/04/05.
+//  Copyright (c) 2013年 Dolice. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FrameByFrameAnimation : UIImageView
+
+@property (nonatomic, retain) UIImageView *animationImageView;
+
+- (void)setAnimating:(int)animationImageNum :(NSString *)animationImageSuffix :(float)animationDuration :(int)animationRepeatCount :(NSInteger)animationImageX :(NSInteger)animationImageY :(NSInteger)animationImageWidth :(NSInteger)animationImageHeight;
+- (NSArray *)animationImages:(NSMutableArray *)animationImageNameList;
++ (UIImage *)getUIImageFromResources:(NSString*)fileName ext:(NSString*)ext;
+
+@end
