aaa4532acf6f7cf3e772db11a4886bd3b1e96e65
Takumi ITO <e125743@Takumi-no-MacBook-Pro-2.local>
2014-01-25 15:33:05 +0900

    キャプチャー

---
+++ Iphone_apps/1:25/cocktail_shaker/Timer-Count/Timer-Count/Start.h
@@ -0,0 +1,13 @@
+//
+//  Start.h
+//  Timer-Count
+//
+//  Created by Takumi ITO on 2014/01/25.
+//  Copyright (c) 2014年 Takumi ITO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Start : UIViewController
+
+@end
---
+++ Iphone_apps/1:25/cocktail_shaker/Timer-Count/Timer-Count/Start.m
@@ -0,0 +1,38 @@
+//
+//  Start.m
+//  Timer-Count
+//
+//  Created by Takumi ITO on 2014/01/25.
+//  Copyright (c) 2014年 Takumi ITO. All rights reserved.
+//
+
+#import "Start.h"
+
+@interface Start ()
+
+@end
+
+@implementation Start
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
